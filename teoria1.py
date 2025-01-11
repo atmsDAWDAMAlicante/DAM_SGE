f1e6bd3386e0715bbb6ac0414917c3d31dcd55b4
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-01-11 19:53:22 +0100

    Commit inicial

---
+++ teoria1.py
@@ -0,0 +1,19 @@
+#Teoría 1
+""" Primeras notas
+"""
+
+# Variables y operadores
+num1 = 1
+num2 = 2
+num3 = num1 + num2
+print(num3)
+
+bool1 = True
+bool2 = False
+if (bool1):
+    #!bool2
+    bool2 = not bool2
+
+bool1 = False
+
+print(bool2)
