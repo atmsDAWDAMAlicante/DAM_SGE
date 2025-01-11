4cfad4c3542f6fe0c2caa3c79a86ee232d370b1a
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-01-11 20:03:06 +0100

    Probando strings y listas

--- teoria1.py
+++ teoria1.py
@@ -17,3 +17,20 @@ if (bool1):
 bool1 = False
 
 print(bool2)
+
+# Formato de los strings
+cancionMiBarba1 = "Mi {0} tiene tres {1}, tres {1} tiene mi {0}, si no tuviera tres {1}, ya no sería una {0}" .format("barba", "pelos")
+print(cancionMiBarba1)
+
+cancionMiBarba2 = "Mi {barba} tiene tres {pelos}, tres {pelos} tiene mi {barba}, si no tuviera tres {pelos}, ya no sería una {barba}" .format(barba="barba", pelos="pelos")
+print(cancionMiBarba2)
+
+#literales
+barba = "barba"
+pelos = "pelos"
+cancionMiBarba3 = f"Mi {barba} tiene tres {pelos}, tres {pelos} tiene mi {barba}, si no tuviera tres {pelos}, ya no sería una {barba}"
+print(cancionMiBarba3)
+
+# Listas
+lista1 = []
+print(len(lista1)) #no hay length
