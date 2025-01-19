2825875345a6b5c27fe65fe03b51a8ea02c4e3a8
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-01-19 10:58:30 +0100

    Ejercicio 8 del bloque II

--- Ejercicios_Python_II/ejercicio08.py
+++ Ejercicios_Python_II/ejercicio08.py
@@ -1,12 +1,16 @@
-
-
-diccionario1 = {
-    "nombre": "Angel",
-    "edad": 54
+usuario = {
+    "nombre": "4",
+    "edad": 0,
+    "direccion": "",
+    "telefono": ""
 }
-lista = list(diccionario1.keys())
-print(lista)
-valores = diccionario1.values()
-print(valores)
-print(diccionario1["nombre"])
-print(diccionario1[f"{lista[0]}"])
+
+listaClaves = list(usuario.keys()) # hay que poner el list para que sea como un array
+print (listaClaves) #=> ['nombre', 'edad', 'direccion', 'telefono']
+
+for i in range (len(listaClaves)):
+    #print (str(listaClaves[i])) no hace falta el str()
+    # print (usuario[listaClaves[i]]) ESTA ES LA CLAVE: ESTO IMPRIME LOS VALORES
+    usuario[listaClaves[i]] = input(f"Introduce {listaClaves[i]}: ") # Con un literal pide cada clave
+# Impresión del resultado con literales
+print (f"{usuario["nombre"]} tiene {usuario["edad"]} años, vive en {usuario["direccion"]} y su número de teléfono es {usuario["telefono"]}")
